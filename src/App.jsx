3bada40ee5aea78ee5704eff3d1f0fe159952a24
ormiller-hilma <ormiller@hilma.tech>
2025-11-10 15:44:03 +0200

    added router library

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
   async function getData() {
@@ -14,11 +18,17 @@ function App() {
     }
   }
 
-  getData();
+  // getData();
 
   return (
     <>
-      <h1></h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
